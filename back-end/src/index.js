9908f47d69eb4ec299eecc23adb2d202f28cbade
gilmarxd <gilmar_custodio_@hotmail.com>
2020-03-31 16:29:03 -0300

    back-end: add cors auth

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
 
 const app = express();
-
 const host = 'localhost';
 const port = 3333;
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
 app.listen(port, host, () => {
     console.log(`Server running on http://${host}:${port}`);
